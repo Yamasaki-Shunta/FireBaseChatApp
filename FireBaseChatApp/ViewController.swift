2e4c256ace10ed6a1f3a3d818d4201252a3f1151
Ymasaki-Sunta <syunta@gmail.com>
2021-02-17 00:42:53 +0900

    Initial Commit

---
+++ FireBaseChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseChatApp
+//
+//  Created by 山﨑隼汰 on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
